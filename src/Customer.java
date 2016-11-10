2f07a1f780cda01f0b7d65627542199f42420b6d
Thomas Herr <thom1810@gmail.com>
2016-11-10 13:07:46 -0500

    Initial Commit

---
+++ src/Customer.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Customer {
+
+    private String firstName;
+
+    public Customer() {
+
+    }
+
+    public Customer(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public ArrayList getAccount() {
+        
+    }
+
+
+
+}
